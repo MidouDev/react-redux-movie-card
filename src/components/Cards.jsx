c8055657925a2da3b4d1f023dfc341f23d0034a5
MidouDev <htmidou@gmail.com>
2018-11-21 16:48:21 +0100

    some styling

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,9 @@
 import React from "react";
+import "../styles/Cards.css";
 
 export const Cards = ({ movieList }) =>
   movieList.map(movie => (
-    <div
-      className="card mb-2"
-      style={{ width: "21rem", height: "100%" }}
-      key={movie.id}
-    >
+    <div className="card mb-2" key={movie.id}>
       <img
         className="card-img-top"
         src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
@@ -14,11 +11,23 @@ export const Cards = ({ movieList }) =>
       />
       <div className="card-body">
         <h5 className="card-title">{movie.title}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{movie.release_date}</h6>
-        <p className="card-text">{movie.overview}</p>
-        <a href="#" className="btn btn-primary">
-          More info
-        </a>
+        <h6 className="card-subtitle mb-2 text-muted">
+          Release date : {movie.release_date}
+        </h6>
+        <h6 className="card-subtitle mb-2 text-muted">
+          Vote count :{" "}
+          <span className="badge badge-secondary">{movie.vote_count}</span>
+        </h6>
+        <h6 className="card-subtitle mb-2 text-muted">
+          Vote average :{" "}
+          <span className="badge badge-primary">{movie.vote_average}</span>
+        </h6>
+
+        <div className="card-footer">
+          <a href="#" className="btn btn-primary btn-block">
+            Movie description
+          </a>
+        </div>
       </div>
     </div>
   ));
